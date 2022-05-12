660958f4c286a88b79b6af9a25521964d75a1503
Serge <sergey.rautbart@gmail.com>
2022-05-12 11:49:12 +0300

    0.3

--- Imaginarium/Assets/MainFolders/Scripts/GameScripts/PScripts/PFieldScript.cs
+++ Imaginarium/Assets/MainFolders/Scripts/GameScripts/PScripts/PFieldScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
 
 public class PFieldScript : MonoBehaviour, IDropHandler
 {
     public Transform hand;
-    public Transform script_handler;
+    public Transform cross_button;
+    public Button ready_button;
 
     public void OnDrop(PointerEventData eventData)
     {
@@ -17,11 +20,23 @@ public class PFieldScript : MonoBehaviour, IDropHandler
             for (int i = 0; i < hand.childCount; i++)
                 hand.GetChild(i).GetComponent<CardScript>().is_used = true;
 
-            script_handler.GetComponent<PActiveElements>().is_active = true;
+            ready_button.interactable = true;
+            cross_button.gameObject.SetActive(true);
 
             card.def_parent = transform;
             card.is_used = true;
             card.transform.position = transform.position;
         }
+    }
+
+    public void OnClickCross()
+    {
+        transform.GetChild(1).SetParent(hand);
+
+        cross_button.gameObject.SetActive(false);
+        ready_button.interactable = false;
+
+        for (int i = 0; i < hand.childCount; i++)
+            hand.GetChild(i).GetComponent<CardScript>().is_used = false;
     }
 }
--- Imaginarium/Assets/MainFolders/Scripts/GameScripts/ReadyScript.cs
+++ Imaginarium/Assets/MainFolders/Scripts/GameScripts/ReadyScript.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ReadyScript : MonoBehaviour
 {
     public Transform next_screen;
+    public TMP_InputField input_field;
+
+    private Button ready_button;
 
     public void OnClick()
     {
         Debug.Log("DONE");
         next_screen.gameObject.SetActive(true);
         transform.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        ready_button = transform.GetComponent<Button>();
+    }
+
+    private void Update()
+    {
+        if(input_field.text != "")
+            ready_button.interactable = true;
+        else
+            ready_button.interactable = false;
     }
 }
