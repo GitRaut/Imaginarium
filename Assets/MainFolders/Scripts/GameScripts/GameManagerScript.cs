be55008101852fe02cf06eba7bf753f10137b769
Serge <sergey.rautbart@gmail.com>
2022-05-15 22:34:25 +0300

    Main menu

---
+++ Assets/MainFolders/Scripts/GameScripts/GameManagerScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public string asoc;
+    public List<Sprite> remaining_cards;
+
+    private void Start()
+    {
+
+    }
+}
